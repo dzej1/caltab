9f5f48a1328f8345f5a463e5fbb14bef60572bbf
dzej1 <dominik.jalc@gmail.com>
2021-01-14 21:07:20 +0100

    render Dashboard in App component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,22 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
-  return (
-    <main>
-      <h1>Create React App + Go API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/master/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Go is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
-}
+/* styles */
+import "./App.css";
+
+/* components */
+//import Login from "./component/Login";
+import Dashboard from "./component/Dashboard";
+
+const App = () => {
+	//TODO: add ability to log in
+	//const [user, setUser] = useState(null);
+
+	return (
+		<div>
+			{/* {user && <Login />} */}
+			<Dashboard />
+		</div>
+	);
+};
 
 export default App;
